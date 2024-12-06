73f34af3497af1f5c0f65555987dac16de017477
Borys Ziukov <ziukoff1985@gmail.com>
2024-12-06 14:36:12 +0200

    added friend-list task

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import userData from "./userData.json";
 import Profile from "../src/components/Profile/Profile";
+import friends from "./friends.json";
+import FriendList from "../src/components/FriendList/FriendList";
 
 const App = () => {
   return (
@@ -11,6 +13,7 @@ const App = () => {
         image={userData.avatar}
         stats={userData.stats}
       />
+      <FriendList friends={friends} />
     </>
   );
 };
